ac093ab6a67e4c6c5cfccbe67c65dac06111cf6a
Mingchung Xia <65872029+mingchungx@users.noreply.github.com>
2024-03-25 23:36:07 -0400

    Update Package.swift to swift tools version 5.9

--- Package.swift
+++ Package.swift
@@ -1,12 +1,28 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Nifty",
-    dependencies: [
-        .Package(url: "https://github.com/nifty-swift/Nifty-libs.git", majorVersion: 1),
+    products: [
+        // Define products here. If Nifty is a library, you might have something like:
+        .library(
+            name: "Nifty",
+            targets: ["Nifty"]),
     ],
-    exclude: [
-    	"Tests/KnownResults",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "version"),
+        .package(url: "https://github.com/nifty-swift/Nifty-libs.git", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Nifty",
+            dependencies: [],
+            exclude: ["Tests/KnownResults"]),
+        .testTarget(
+            name: "NiftyTests",
+            dependencies: ["Nifty"]),
     ]
-
 )
